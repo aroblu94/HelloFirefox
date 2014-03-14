be3f30e610ecb04287263c07a988dc51579e84fd
giuscri <giuscri@gmail.com>
2014-03-14 20:08:28 +0100

    Populating ...

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstVariable = "Mum!, no hands!!"
+
+/* Open Javascript console in your browser for next instruction */
+// ^^^^^^^^^^^^^^^^^^^^^^^ Ctrl+Shift+J in Chromium ...
+console.log("firstVariable is: \""+firstVariable+"\"")
+
+/* Manipulating index.html source code */
+//...
